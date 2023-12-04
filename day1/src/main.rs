802a8004c2b422c61214c2308a3ff6e0050db929
Aaron <42575013+SuperWinner50@users.noreply.github.com>
2023-12-03 21:40:57 -0500

    fix

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -33,24 +33,6 @@ fn p2() -> u32 {
         .sum()
 }
 
-fn p2_better() -> u32 {
-    std::fs::read_to_string("input.txt").unwrap()
-        .lines()
-        .map(|line| {
-            let nums = (0..line.len()).filter_map(|i|
-                line[i..i + 1].parse::<u32>().ok().or(
-                    NUMS.iter()
-                        .enumerate()
-                        .find(|(_, &num)| line[i..].starts_with(num))
-                        .map(|n| n.0 as u32 + 1)
-                )
-            ).collect::<Vec<_>>();
-
-            nums[0] * 10 + nums[nums.len() - 1]
-        })
-        .sum()
-}
-
 fn main() {
     println!("{}", p2());
 }
